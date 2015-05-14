032891912ef9aa115d27714ed2c55da17cced217
David Dias <daviddias.p@gmail.com>
2015-05-15 00:43:31 +0100

    Initial Commit

---
+++ toPDF.sh
@@ -0,0 +1,7 @@
+# How to?
+# Run once with /bibliography not commented in the individual section, then run again after commenting those lines
+
+pdflatex index 
+bibtex index 
+pdflatex index 
+pdflatex index
